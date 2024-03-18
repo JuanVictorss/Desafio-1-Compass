acbb45321dbf3c56e284904b0f9cd5c102d78aaf
Juan Victor <juanvictoficial@gmail.com>
2024-03-18 12:11:37 -0300

    data e hora

--- Challenge 1 - Juan Victor/home.js
+++ Challenge 1 - Juan Victor/home.js
@@ -15,5 +15,49 @@ function getTemperatura(cidade) {
       document.getElementById("cidade").innerHTML = local;
     });
 }
+
 const profile = JSON.parse(sessionStorage.getItem("profile"));
 getTemperatura(profile.city);
+
+const atual = new Date();
+
+function dataHora() {
+  const diaSemana = [
+    "Domingo",
+    "Segunda-feira",
+    "Terça-feira",
+    "Quarta-feira",
+    "Quinta-feira",
+    "Sexta-feira",
+    "Sábado",
+  ];
+  const mesAno = [
+    "janeiro",
+    "fevereiro",
+    "março",
+    "abril",
+    "maio",
+    "junho",
+    "julho",
+    "agosto",
+    "setembro",
+    "outubro",
+    "novembro",
+    "dezembro",
+  ];
+
+  const hora = atual.getHours().toString().padStart(2, "0");
+  const minuto = atual.getMinutes().toString().padStart(2, "0");
+  stringHora = hora + ":" + minuto;
+
+  document.getElementById("hora").textContent = stringHora;
+
+  const dia = atual.getDate().toString().padStart(2, "0");
+
+  document.getElementById("data").textContent = `${diaSemana[atual.getDay()]},
+     ${dia} de ${mesAno[atual.getMonth()]}
+   de ${atual.getFullYear()}`;
+}
+
+setInterval(dataHora, 1000);
+dataHora();
